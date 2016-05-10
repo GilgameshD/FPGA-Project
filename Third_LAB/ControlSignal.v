7263487f473105a93dea8cf16db05d2ce050e9a3
Wenhao Ding <815128388@qq.com>
2016-05-10 21:57:38 +0800

    Add files via upload

--- Third_LAB/ControlSignal.v
+++ Third_LAB/ControlSignal.v
@@ -12,25 +12,23 @@ module ControlSignal(clkControl, testMode, modeControl, enable, clear, latch);
     enable <= 0;
     clear <= 1;
     latch <= 1;
-	  oldModeControl <= 1;
   end
 
   // every time when the clk reaches
   always @(posedge clkControl) begin
   // when mode changed we should count  
     if(testMode != oldMode || oldModeControl != modeControl) begin  
-      latch = 0;    // dont lock
-      clear = 0;    // clear to be ready to count
-      enable = 1;   // start to count
+      latch <= 0;    // dont lock
+      clear <= 0;    // clear to be ready to count
+      enable <= 1;   // start to count
       // mode changed
-      oldMode = testMode;
-      oldModeControl = modeControl;
+      oldMode <= testMode;
+      oldModeControl <= modeControl;
     end
     else begin
-      enable = 0; // disable to count
-      clear = 1;  // don't clear
-      latch = 1;  // lock the number
+      enable <= 0; // disable to count
+      clear <= 1;  // don't clear
+      latch <= 1;  // lock the number
     end
   end
 endmodule
-
